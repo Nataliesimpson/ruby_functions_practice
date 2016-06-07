3b4b242d13ea2716219704a3678133fed7ed034a
Natalie <natzsimpson@hotmail.com>
2016-06-07 16:10:31 +0100

    adds 5 tests

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -5,4 +5,16 @@ end
 def add(first_number, second_number)
   return first_number + second_number
 end
-  
+
+def subtract(first_number, second_number)
+   return first_number - second_number
+end
+
+def multiply(x, y)
+   return x * y
+end
+
+def divide(a, b)
+  return a / b
+end
+
