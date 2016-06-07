101b5e2448160b5cf96f189610ad1f0d9c2d7534
Natalie <natzsimpson@hotmail.com>
2016-06-07 16:37:31 +0100

    adds 8 tests

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -18,3 +18,14 @@ def divide(a, b)
   return a / b
 end
 
+def length_of_string(test_string)
+   return test_string.length
+end
+
+def join_string(string_1, string_2)
+    return string_1 + string_2
+end
+
+def add_string_as_number(x, y)
+   return x.to_i + y.to_i
+end   
